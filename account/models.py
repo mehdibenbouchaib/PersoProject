be17bbe30496432d379c2f5f7bcd878cc352e8a0
mehdibenbouchaib <mehdi.benbouchaib@gmail.com>
2022-12-06 00:01:22 +0100

    "create a manager field for account"

--- account/models.py
+++ account/models.py
@@ -1,34 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser
 
-
-class MyAccountManager(BaseUserManager):
-    def create_user(self, email, username, password=None):
-        if not email:
-            raise ValueError("Users must have an email address")
-        if not username:
-            raise ValueError("Users must have an username")
-
-        user = self.model(
-            email=self.normalize_email(email),
-            username=username,
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, username, password):
-        user = self.create_user(
-            email=self.normalize_email(email),
-            password=password,
-            username=username,
-
-        )
-        user.is_admin = True
-        user.is_staff = True
-        user.is_superuser = True
-        user.save(using=self._db)
-        return user
+from account.managers import MyAccountManager
 
 
 class Account(AbstractBaseUser):
